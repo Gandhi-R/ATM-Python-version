4f13605e4039e4ba7c194445be184b574b270572
Gandhi <gandhirahmawan@gmail.com>
2026-01-09 20:25:40 +0700

    Fix withdrawal nominal validation logic and Update tarik saldo to use centralized language handler

--- utils/language.py
+++ utils/language.py
@@ -14,6 +14,26 @@ TEXT = {
         "invalid_input":"PILIHAN TIDAK VALID",
         "info_saldo": "SALDO ANDA SAAT INI",
         "press_enter": "Tekan ENTER untuk melanjutkan",
+        
+
+        #tarik saldo
+        "menu_tarik":"PILIH NOMINAL PENARIKAN TUNAI",
+        "note_tarik":"INPUT ANGKA 0\n  \t\tUNTUK MEMBANTALKAN TRANSAKSI",
+        "input_tarik":"\tMASUKKAN PENARIKAN PILIHAN PENARIKAN NOMINAL : ",
+        "note_tarik":"NOMINAL PENARIKAN UANG TUNAI\n  \tHARUS KELIPATAN Rp.50.000 DAN MAKSIMAL Rp.2.500.000 ",
+        "not_proses":"MAAF TRANSAKSI TIDAK DAPAT DIPROSES.",
+        "left_tarik":"ANDA KELUAR PROGRAM",
+
+        #hitung_tarik saldo
+        "min_saldo":"MAAF, SALDO ANDA KURANG.",
+        "inpo_saldo":"ANDA TELAH MENARIK UANG TUNAI SEBESAR",
+        "sisa":"SISA SALDO ANDA SEBESAR",
+        "struk":"APA ANDA INGIN MENCETAK STRUK TRANSAKSI ANDA ?",
+        "valid_saldo":"STRUK ANDA SEDANG DICETAK. SILAHKAN AMBIL STRUK ANDA DI NOTEPAD",
+       
+
+       
+        
 
         #cetak struk
         "tanggal":"TANGGAL",
@@ -37,6 +57,23 @@ TEXT = {
         "invalid_input":"INVALID INPUT",
         "info_saldo": "YOUR CURRENT BALANCE",
         "press_enter": "Press ENTER to continue",
+
+        
+        #tarik saldo
+        "menu_tarik":"SELECT CASH WITHDRAWAL NOMINAL",
+        "note_tarik":"ENTER NUMBER 0\n  \t\tTO BLOW UP TRANSACTIONS",
+        "input_tarik":"\tENTER WITHDRAWAL OPTION NOMINAL WITHDRAWAL OPTION : ",
+        "note_tarik":"CASH WITHDRAWAL NOMINAL\n  \tMUST BE MULTIPLE IDR 50,000 AND MAXIMUM IDR 2,500,000",
+        "not_proses":"SORRY, THE TRANSACTION CANNOT BE PROCESSED.",
+        "left_tarik":"YOUR EXIT THE PROGRAM",
+
+        #hitung tarik saldo
+        "min_saldo":"SORRY, YOUR BALANCE IS LOW",
+        "inpo_saldo":"YOU HAVE WITHDRAWALED THE AMOUNT OF CASH",
+        "sisa":"YOUR REMAINING BALANCE IS",
+        "struk":"DO YOU WANT TO PRINT YOUR TRANSACTION RECEIPT?",
+        "valid_saldo":"YOUR REQUEST IS BEING PRINTED. PLEASE TAKE YOUR RECEIPT IN A NOTEPAD",
+
 
         #cetak_struk
         "tanggal":"DATE",
