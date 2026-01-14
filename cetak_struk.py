125c87002c035997355b3f42d70a65075e4ec3a3
Gandhi <gandhirahmawan@gmail.com>
2026-01-14 23:05:30 +0700

    Add multi-language support to transfer receipt and update README

--- cetak_struk.py
+++ cetak_struk.py
@@ -11,11 +11,11 @@ def cetak_tarik_saldo(pemilik,nominal,bahasa):
     waktu=sekarang.strftime("%H:%M:%S")
     with open("struk_saldo.txt","w") as file:
         file.write("=====================ATM BERSAMA=====================\n\n")
-        file.write(f"{TEXT[bahasa]['tanggal']}    : {tanggal}                       WAKTU : {waktu}\n")
+        file.write(f"{TEXT[bahasa]['tanggal']}    : {tanggal}                       {TEXT[bahasa]['waktu']}: {waktu}\n")
         file.write(f"{TEXT[bahasa]['nama']}       : {pemilik['namaPemilik']}\n")
         file.write(f"{TEXT[bahasa]['no_rekening']}        : {pemilik['nomorRekening']}\n")
-        file.write(f"{TEXT[bahasa]['tarik']}              : Rp.{nominal} \n")
-        file.write(f"{TEXT[bahasa]['sisa']}            : {pemilik['saldo']}\n\n")
+        file.write(f"{TEXT[bahasa]['tarik']}             : Rp.{nominal} \n")
+        file.write(f"{TEXT[bahasa]['sisa']}         : {pemilik['saldo']}\n\n")
         file.write("======================================================\n")
         file.write(f"                    {TEXT[bahasa]['deskrip_1']} \n")
         file.write(f"                  {TEXT[bahasa]['deskrip_2']} ")
@@ -26,23 +26,23 @@ def cetak_tarik_saldo(pemilik,nominal,bahasa):
  
 
 
-def cetak_transfer_saldo(pemilik,nominal,tujuan,lang):
+def cetak_transfer_saldo(pemilik,nominal,tujuan,bahasa):
     sekarang=datetime.datetime.now()
     tanggal=sekarang.strftime("%d-%m-%y")
     waktu=sekarang.strftime("%H:%M:%S")
     with open("struk_transfer_saldo.txt","w") as file:
         file.write("=====================ATM BERSAMA====================\n\n")
-        file.write(f"TANGGAL : {tanggal}                        WAKTU : {waktu}\n")
-        file.write(f"NAMA PENGIRIM      : {pemilik['namaPemilik']}\n")
-        file.write(f"NO REK PENIGIRIM   : {pemilik['nomorRekening']}\n")
-        file.write(f"NAMA PENERIMA      : {tujuan['namaPemilik']}\n")
-        file.write(f"NO REK PENERIMA    : {tujuan['nomorRekening']}\n")
-        file.write(f"TRANSFER           : Rp.{nominal}\n")
-        file.write(f"SALDO PENGIRIM     : Rp.{pemilik['saldo']}\n")
-        file.write(f"SALDO PENERIMA     : Rp.{tujuan['saldo']}\n")
+        file.write(f"{TEXT[bahasa]['tanggal']}L : {tanggal}                        {TEXT[bahasa]['waktu']}: {waktu}\n")
+        file.write(f"{TEXT[bahasa]['pengirim']}           : {pemilik['namaPemilik']}\n")
+        file.write(f"{TEXT[bahasa]['no_pengirim']}   : {pemilik['nomorRekening']}\n")
+        file.write(f"{TEXT[bahasa]['penerima']}           : {tujuan['namaPemilik']}\n")
+        file.write(f"{TEXT[bahasa]['no_penerima']}    : {tujuan['nomorRekening']}\n")
+        file.write(f"{TEXT[bahasa]['tf']}                : Rp.{nominal}\n")
+        file.write(f"{TEXT[bahasa]['saldo_pener']}          : Rp.{pemilik['saldo']}\n")
+        file.write(f"{TEXT[bahasa]['saldo_pengi']}          : Rp.{tujuan['saldo']}\n")
         file.write("=====================================================\n")
-        file.write("                 SIMPAN TANDA TERIMA INI \n")
-        file.write("            SEBAGAI BUKTI TRANSAKSI YANG SAH \n\n\n")
+        file.write(f"                 {TEXT[bahasa]['deskrip_1']} \n")
+        file.write(f"            {TEXT[bahasa]['deskrip_2']} \n\n\n")
 
         os.system("start /min notepad struk_transfer_saldo.txt")
 
