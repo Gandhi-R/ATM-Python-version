0c7af5dadf3e9ecea27a1a106a4918b31b2298e0
Gandhi <gandhirahmawan@gmail.com>
2026-01-18 14:11:51 +0700

    Refactor language dictionary for consistency and scalability

--- utils/language.py
+++ utils/language.py
@@ -1,4 +1,161 @@
+
 TEXT = {
+    "id": {
+
+        # ===== MAIN MENU =====
+        "menu_title": "PILIH TRANSAKSI",
+        "menu_balance": "1. INFO SALDO",
+        "menu_withdraw": "2. TARIK TUNAI",
+        "menu_transfer": "3. TRANSFER",
+        "menu_change_pin": "4. GANTI PIN",
+        "menu_history": "5. RIWAYAT TRANSAKSI",
+        "menu_exit": "6. KELUAR",
+
+        "exit_message": "ANDA TELAH KELUAR PROGRAM\n\tTERIMA KASIH TELAH MENGGUNAKAN LAYANAN KAMI",
+        "choice_input": "MASUKKAN PILIHAN ANDA",
+        "invalid_input": "PILIHAN TIDAK VALID",
+        "input_error": "INPUT TIDAK VALID. MASUKKAN ANGKA YANG BENAR",
+        "current_balance": "SALDO ANDA SAAT INI",
+        "press_enter": "Tekan ENTER untuk melanjutkan",
+
+        # ===== WITHDRAW =====
+        "withdraw_menu": "PILIH NOMINAL PENARIKAN TUNAI",
+        "withdraw_note": "NOMINAL HARUS KELIPATAN Rp.50.000 DAN MAKSIMAL Rp.2.500.000",
+        "withdraw_cancel_note": "INPUT ANGKA 0 UNTUK MEMBATALKAN TRANSAKSI",
+        "withdraw_input": "MASUKKAN PILIHAN PENARIKAN : ",
+        "withdraw_cancelled": "ANDA MEMBATALKAN TRANSAKSI",
+        "transaction_failed": "MAAF, TRANSAKSI TIDAK DAPAT DIPROSES",
+
+        # ===== WITHDRAW RESULT =====
+        "balance_not_enough": "MAAF, SALDO ANDA KURANG",
+        "withdraw_success": "ANDA TELAH MENARIK UANG TUNAI SEBESAR",
+        "remaining_balance": "SISA SALDO ANDA SEBESAR",
+        "print_receipt": "APA ANDA INGIN MENCETAK STRUK TRANSAKSI?",
+        "receipt_printed": "STRUK SEDANG DICETAK. SILAHKAN AMBIL STRUK ANDA",
+
+        # ===== TRANSFER =====
+        "transfer_title": "TRANSFER SALDO KE REKENING LAIN",
+        "transfer_cancel_note": "INPUT ANGKA 0 UNTUK MEMBATALKAN TRANSAKSI",
+        "transfer_input_account": "MASUKKAN NOMOR REKENING TUJUAN",
+        "transfer_invalid_account": "NOMOR REKENING HANYA BOLEH BERISI ANGKA",
+        "transfer_same_account": "REKENING TUJUAN TIDAK BOLEH SAMA DENGAN REKENING ANDA",
+        "transfer_account_not_found": "NOMOR REKENING TIDAK DITEMUKAN",
+        "transfer_registered_account": "REKENING TERDAFTAR",
+        "account_owner": "NAMA PEMILIK",
+        "account_number": "NOMOR REKENING",
+        "transfer_nominal_note": "NOMINAL HARUS KELIPATAN Rp.50.000 DAN MAKSIMAL Rp.2.500.000",
+        "transfer_input_nominal": "MASUKKAN NOMINAL TRANSFER : Rp.",
+        "transfer_success": "TRANSFER BERHASIL",
+        "transfer_remaining_balance": "SISA SALDO ANDA SAAT INI Rp.",
+
+        # ===== CHANGE PIN =====
+        "change_pin_title": "GANTI PIN",
+        "old_pin_input": "MASUKKAN PIN LAMA",
+        "pin_length_error": "PIN HARUS 6 DIGIT",
+        "wrong_pin": "PIN SALAH! SISA PERCOBAAN",
+        "new_pin_input": "MASUKKAN PIN BARU",
+        "same_pin_error": "PIN BARU TIDAK BOLEH SAMA DENGAN PIN LAMA",
+        "weak_pin": "PIN TERLALU LEMAH",
+        "confirm_pin": "KONFIRMASI PIN BARU",
+        "pin_not_match": "KONFIRMASI PIN TIDAK COCOK",
+        "pin_changed": "PIN BERHASIL DIUBAH",
+        "pin_blocked": "AKUN TERKUNCI KARENA SALAH PIN 3 KALI",
+
+        # ===== RECEIPT (WITHDRAW) =====
+        "receipt_date": "TANGGAL",
+        "receipt_time": "WAKTU",
+        "receipt_owner": "NAMA PEMILIK",
+        "receipt_account": "NO REKENING",
+        "receipt_amount": "JUMLAH",
+        "receipt_balance": "SISA SALDO",
+        "receipt_note_1": "SIMPAN STRUK INI",
+        "receipt_note_2": "SEBAGAI BUKTI TRANSAKSI YANG SAH",
+
+        # ===== RECEIPT (TRANSFER) =====
+        "sender_account": "NO REKENING PENGIRIM",
+        "receiver_name": "NAMA PENERIMA",
+        "receiver_account": "NO REKENING PENERIMA",
+        "sender_balance": "SALDO PENGIRIM",
+        "receiver_balance": "SALDO PENERIMA",
+    },
+
+    "en": {
+
+        # ===== MAIN MENU =====
+        "menu_title": "SELECT TRANSACTION",
+        "menu_balance": "1. BALANCE INFO",
+        "menu_withdraw": "2. WITHDRAW",
+        "menu_transfer": "3. TRANSFER",
+        "menu_change_pin": "4. CHANGE PIN",
+        "menu_history": "5. TRANSACTION HISTORY",
+        "menu_exit": "6. EXIT",
+
+        "exit_message": "YOU HAVE EXITED THE PROGRAM\nTHANK YOU FOR USING OUR SERVICES",
+        "choice_input": "ENTER YOUR CHOICE",
+        "invalid_input": "INVALID INPUT",
+        "input_error": "INVALID INPUT. PLEASE ENTER A VALID NUMBER",
+        "current_balance": "YOUR CURRENT BALANCE",
+        "press_enter": "Press ENTER to continue",
+
+        # ===== WITHDRAW =====
+        "withdraw_menu": "SELECT CASH WITHDRAWAL AMOUNT",
+        "withdraw_note": "MUST BE MULTIPLE OF IDR 50,000 AND MAXIMUM IDR 2,500,000",
+        "withdraw_cancel_note": "ENTER 0 TO CANCEL TRANSACTION",
+        "withdraw_input": "ENTER WITHDRAWAL OPTION : ",
+        "withdraw_cancelled": "TRANSACTION CANCELLED",
+        "transaction_failed": "SORRY, TRANSACTION CANNOT BE PROCESSED",
+
+        # ===== WITHDRAW RESULT =====
+        "balance_not_enough": "SORRY, YOUR BALANCE IS NOT ENOUGH",
+        "withdraw_success": "YOU HAVE WITHDRAWN",
+        "remaining_balance": "YOUR REMAINING BALANCE IS",
+        "print_receipt": "DO YOU WANT TO PRINT THE RECEIPT?",
+        "receipt_printed": "RECEIPT IS BEING PRINTED",
+
+        # ===== TRANSFER =====
+        "transfer_title": "TRANSFER BALANCE TO ANOTHER ACCOUNT",
+        "transfer_cancel_note": "ENTER 0 TO CANCEL TRANSACTION",
+        "transfer_input_account": "ENTER DESTINATION ACCOUNT NUMBER",
+        "transfer_invalid_account": "ACCOUNT NUMBER MUST BE NUMERIC",
+        "transfer_same_account": "DESTINATION ACCOUNT CANNOT BE THE SAME",
+        "transfer_account_not_found": "DESTINATION ACCOUNT NOT FOUND",
+        "transfer_registered_account": "REGISTERED ACCOUNT",
+        "account_owner": "ACCOUNT OWNER",
+        "account_number": "ACCOUNT NUMBER",
+        "transfer_nominal_note": "MUST BE MULTIPLE OF IDR 50,000 AND MAXIMUM IDR 2,500,000",
+        "transfer_input_nominal": "ENTER TRANSFER AMOUNT : IDR",
+        "transfer_success": "TRANSFER SUCCESSFUL",
+        "transfer_remaining_balance": "YOUR REMAINING BALANCE IS IDR",
+
+        # ===== CHANGE PIN =====
+        "change_pin_title": "CHANGE PIN",
+        "old_pin_input": "ENTER OLD PIN",
+        "pin_length_error": "PIN MUST BE 6 DIGITS",
+        "wrong_pin": "WRONG PIN! ATTEMPTS LEFT",
+        "new_pin_input": "ENTER NEW PIN",
+        "same_pin_error": "NEW PIN MUST BE DIFFERENT",
+        "weak_pin": "PIN TOO WEAK",
+        "confirm_pin": "CONFIRM NEW PIN",
+        "pin_not_match": "PIN CONFIRMATION DOES NOT MATCH",
+        "pin_changed": "PIN CHANGED SUCCESSFULLY",
+        "pin_blocked": "ACCOUNT LOCKED DUE TO 3 FAILED ATTEMPTS",
+
+        # ===== RECEIPT =====
+        "receipt_date": "DATE",
+        "receipt_time": "TIME",
+        "receipt_owner": "OWNER NAME",
+        "receipt_account": "ACCOUNT NUMBER",
+        "receipt_amount": "AMOUNT",
+        "receipt_balance": "REMAINING BALANCE",
+        "receipt_note_1": "KEEP THIS RECEIPT",
+        "receipt_note_2": "AS LEGAL PROOF",
+    }
+}
+
+
+"""
+TEXT = {
+    
     "id": {
         #main
         "menu_title": "PILIH TRANSAKSI",
@@ -91,7 +248,8 @@ TEXT = {
 
     },
 
-    "en": {
+    "en": 
+    {
 
         #main
         "menu_title": "SELECT TRANSACTION",
@@ -177,7 +335,9 @@ TEXT = {
         "tf":"TRANSFER",
         "saldo_pener":"SENDER BALANCE",
         "saldo_pengi":"RECIPIENT BALANCE",
-
-    
     }
-}
+  
+} """
+
+
+
